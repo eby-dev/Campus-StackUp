bacfafe20da8426e5c7be7f53da3f331b45341c4
Ahmad Abu Hasan <36786100+eby8zevin@users.noreply.github.com>
2024-06-26 20:30:16 +0700

    Let’s Ace It! Preparing Your Submissions!

--- Building an API Client Library with Rust I/Quest 2 - Building The Library/endoflife/src/main.rs
+++ Building an API Client Library with Rust I/Quest 2 - Building The Library/endoflife/src/main.rs
@@ -1,13 +1,5 @@
-use endoflife::rust;
-use endoflife::request::api_request_single_rust_cycle;
+use endoflife::request::api_request_all_rust_cycles;
 
 fn main() {
-    let rust_version = "1.78";
-    let data = api_request_single_rust_cycle(rust_version).unwrap();
-
-    println!("{:#?}", data);
-
-    let json_str = serde_json::to_string_pretty(&data).unwrap();
-
-    println!("{}", &json_str);
+    let _all_cycles = api_request_all_rust_cycles().unwrap();
 }
--- Building an API Client Library with Rust I/Quest 2 - Building The Library/endoflife/src/request.rs
+++ Building an API Client Library with Rust I/Quest 2 - Building The Library/endoflife/src/request.rs
@@ -23,3 +23,18 @@ pub fn api_request_single_rust_cycle(
     let deserialised_response = serde_json::from_str::<RustSingleCycle>(&response)?;
     Ok(deserialised_response)
 }
+
+pub fn api_request_all_rust_cycles() -> Result<Vec<RustSingleCycle>, Box<dyn Error>> {
+    let request_string: &str = "https://endoflife.date/api/rust.json";
+
+    // Builds the client request
+    let client = Client::new();
+
+    // Our response body
+    let body = client.get(request_string).header(ACCEPT, HEADER).send()?.bytes()?.to_vec();
+
+    // Our response string
+    let response = unsafe { String::from_utf8_unchecked(body) };
+    let deserialised_response: Vec<RustSingleCycle> = serde_json::from_str(&response)?;
+    Ok(deserialised_response)
+}
