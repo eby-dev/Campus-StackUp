13e0e15dc38dcfe8a08d68d4ce3e1f8f6b3ceb54
Ahmad Abu Hasan <36786100+eby8zevin@users.noreply.github.com>
2024-07-11 22:45:28 +0700

    Creating a Templated Web Application

--- Developing Web Applications with Flask/FlaskCampaign/Quest2/app/app.py
+++ Developing Web Applications with Flask/FlaskCampaign/Quest2/app/app.py
@@ -1,11 +1,36 @@
 from flask import Flask, request, render_template
 app = Flask(__name__, template_folder='templates', static_folder='static')
+
 username = "ahmadabuhasan"
 description = "I love developing web applications."
 description += "And this text was created with Jinja"
+projects = [
+    {
+        'id': 1,
+        'title': 'E-commerce Website',
+        'description': 'A website where you can buy stuff',
+        'author': 'ahmadabuhasan',
+        'category': 'Web Development'
+    },
+    {
+        'id': 2,
+        'title': 'Flask Website',
+        'description': 'A website made with Flask',
+        'author': 'ahmadabuhasan',
+        'category': 'Web Development'
+    },
+    {
+        'id': 3,
+        'title': 'Cool Mobile App',
+        'description': 'An app that does cool stuff',
+        'author': 'ahmadabuhasan',
+        'category': 'Mobile Development'
+    }
+]
+
 @app.route('/')
 def hello_world():
-    return render_template('index.html', username = username, description = description)
+    return render_template('index.html', username = username, description = description, projects = projects)
 
 @app.route('/about')
 def about():
