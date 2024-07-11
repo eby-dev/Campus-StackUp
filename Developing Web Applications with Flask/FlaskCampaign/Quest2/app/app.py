b32577c59e0ec47d1d96e2e3feca51aea65cf9ac
Ahmad Abu Hasan <36786100+eby8zevin@users.noreply.github.com>
2024-07-11 22:19:41 +0700

    Routing with Flask

---
+++ Developing Web Applications with Flask/FlaskCampaign/Quest2/app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/about')
+def about():
+    return 'About Page'
+
+@app.route('/contact', methods = ['POST', 'GET'])
+def contact():
+    if request.method == 'POST':
+        return 'You are using POST'
+    elif request.method == 'GET':
+        return 'You are using GET'
+
+if __name__ == '__main__':
+    app.run()
